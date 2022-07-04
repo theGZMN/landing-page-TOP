a6aa691c3280190ee3141284cdb04496b2f06091
Gozman <thegzmn@gmail.com>
2022-07-04 23:02:49 +0100

    Create js/main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$('#locations a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 80
+      },
+      800
+    );
+  }
+});
